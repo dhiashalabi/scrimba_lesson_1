fa5aed6d6827d92381751c466c564f9ca1e21d1e
9iaashalaby <30384731+9iaashalaby@users.noreply.github.com>
2021-11-04 11:34:23 -0700

    [Add] header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+    return (
+        <nav>
+            <h1>Hello a third time!</h1>
+            <ul>
+                <li>Thing 1</li>
+                <li>Thing 2</li>
+                <li>Thing 3</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Header;

e914d46389962212b0958f5a62afe4e4eefaa5c3
9iaashalaby <30384731+9iaashalaby@users.noreply.github.com>
2021-11-04 11:34:00 -0700

    [Add] main component

---
+++ src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainContent() {
+    return (
+        <main>
+            <p>This is where most of my content will go...</p>
+        </main>
+    );
+}
+
+export default MainContent;
